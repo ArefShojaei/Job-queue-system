0d9b32d724a146d538769a9644e1e13801253978
ArefShojaei <arefshojaei82@gmail.com>
2026-06-17 23:16:47 +0330

    feat: add Job command

--- app/Console/Commands/Job/FailedJobListCommand.php
+++ app/Console/Commands/Job/FailedJobListCommand.php
@@ -5,11 +5,11 @@ namespace App\Console\Commands\Job;
 use PhpX\Components\Console\Command;
 use Illuminate\Database\Capsule\Manager as DB;
 
-final class JobListCommand extends Command
+final class FailedJobListCommand extends Command
 {
     public function exec(array $params): string
     {
-        $jobs = DB::table("jobs")->orderBy("id")->get();
+        $jobs = DB::table("failed_jobs")->orderBy("id")->get();
 
         return print_r($jobs->toArray(), true);
     }
--- app/Console/Commands/Job/JobTestCommand.php
+++ app/Console/Commands/Job/JobTestCommand.php
@@ -1,21 +1,32 @@
 <?php
 
-namespace App\Console\Commands;
+namespace App\Console\Commands\Job;
 
 use PhpX\Components\Console\Command;
 use PhpX\Utils\Console\Console;
 use Illuminate\Database\Capsule\Manager as DB;
 use Core\Enums\JobStatus;
 use App\Jobs\SendMailJob;
+use App\Jobs\SendWelcomeMessageJob;
 
 final class JobTestCommand extends Command
 {
     public function exec(array $params): string
     {
         $job = DB::table("jobs")->insert([
-            "payload" => serialize(new SendMailJob("aref@gmail.com")),
-            "status" => JobStatus::PENDING,
+            [
+                "payload" => serialize(new SendMailJob("aref@gmail.com")),
+                "status" => JobStatus::PENDING,
+            ],
+            [
+                "payload" => serialize(new SendWelcomeMessageJob()),
+                "status" => JobStatus::PENDING,
+            ],
         ]);
+        // $job = DB::table("jobs")->insert([
+        //     "payload" => serialize(new SendMailJob("aref@gmail.com")),
+        //     "status" => JobStatus::PENDING,
+        // ]);
 
         return $job
             ? Console::success("Added.", "JOB")
